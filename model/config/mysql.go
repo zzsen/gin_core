9aa10c6c8391182ad872ac9f5af0908ea3dc87b2
zzsen <296099296@qq.com>
2025-01-15 21:36:24 +0800

    feat: mysql config support loc config

--- model/config/mysql.go
+++ model/config/mysql.go
@@ -9,6 +9,7 @@ type DbInfo struct {
 	DBName        string   `yaml:"dbName"`        // 数据库名
 	Username      string   `yaml:"username"`      // 数据库账号
 	Password      string   `yaml:"password"`      // 数据库密码
+	Loc           string   `yaml:"loc"`           // 时区
 	MaxIdleConns  int      `yaml:"maxIdleConns"`  // 空闲中的最大连接数
 	MaxOpenConns  int      `yaml:"maxOpenConns"`  // 打开到数据库的最大连接数
 	Migrate       string   `yaml:"migrate"`       // 每次启动时更新数据库表的方式 update:增量更新表，create:删除所有表再重新建表, 其他则不执行任何动作
@@ -19,10 +20,14 @@ type DbInfo struct {
 }
 
 func (dbInfo *DbInfo) Dsn() string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+	if dbInfo.Loc == "" {
+		dbInfo.Loc = "Local"
+	}
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=%s",
 		dbInfo.Username,
 		dbInfo.Password,
 		dbInfo.Host,
 		dbInfo.Port,
-		dbInfo.DBName)
+		dbInfo.DBName,
+		dbInfo.Loc)
 }
