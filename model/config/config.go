4b7e0c4081180586d4640b534c6e744d4536ff77
zzsen <296099296@qq.com>
2025-02-17 21:03:22 +0800

    feat: update log config model

--- model/config/config.go
+++ model/config/config.go
@@ -1,19 +1,15 @@
 package config
 
-import (
-	"github.com/zzsen/gin_core/logger"
-)
-
 type BaseConfig struct {
-	System      SystemInfo           `yaml:"system"`
-	Service     ServiceInfo          `yaml:"service"`
-	Log         logger.LoggersConfig `yaml:"log"`
-	Db          DbInfo               `yaml:"db"`
-	DbList      []DbInfo             `yaml:"dbList"`
-	DbResolvers DbResolvers          `yaml:"dbResolvers"`
-	Redis       RedisInfo            `yaml:"redis"`
-	RedisList   []RedisInfo          `yaml:"redisList"`
-	RabbitMQ    RabbitMQInfo         `yaml:"rabbitMQ"`
-	Es          EsInfo               `yaml:"es"`
-	Smtp        SmtpInfo             `yaml:"smtp"`
+	System      SystemInfo    `yaml:"system"`
+	Service     ServiceInfo   `yaml:"service"`
+	Log         LoggersConfig `yaml:"log"`
+	Db          DbInfo        `yaml:"db"`
+	DbList      []DbInfo      `yaml:"dbList"`
+	DbResolvers DbResolvers   `yaml:"dbResolvers"`
+	Redis       RedisInfo     `yaml:"redis"`
+	RedisList   []RedisInfo   `yaml:"redisList"`
+	RabbitMQ    RabbitMQInfo  `yaml:"rabbitMQ"`
+	Es          EsInfo        `yaml:"es"`
+	Smtp        SmtpInfo      `yaml:"smtp"`
 }
