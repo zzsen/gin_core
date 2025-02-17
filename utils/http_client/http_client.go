a002aede33a7959ac6ccb981d24d4d453e682690
zzsen <296099296@qq.com>
2025-02-17 21:05:34 +0800

    feat: http client add form request

--- utils/http_client/http_client.go
+++ utils/http_client/http_client.go
@@ -5,8 +5,10 @@ package http_client
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -68,6 +70,65 @@ func PostJson(url string, body string, headers map[string]string, timeout int) R
 	return request(req, timeout)
 }
 
+func PostForm(httpUrl string, data map[string]string, filenames []string,
+	headers map[string]string, timeout int) ResponseWrapper {
+	os.Setenv("GODEBUG", "tlsrsakex=1")
+	// 创建一个字节缓冲区用于存储表单数据
+	body := &bytes.Buffer{}
+	// 创建一个 multipart 写入器，用于向缓冲区写入表单数据
+	writer := multipart.NewWriter(body)
+
+	// 遍历表单数据，将普通表单字段写入
+	for key, value := range data {
+		err := writer.WriteField(key, value)
+		if err != nil {
+			return ResponseWrapper{0, fmt.Sprintf("写入表单字段 %s 失败: %s", key, err.Error()), make(http.Header)}
+		}
+	}
+
+	for _, filename := range filenames {
+		// 打开要上传的文件
+		file, err := os.Open(filename)
+		if err != nil {
+			return ResponseWrapper{0, fmt.Sprintf("打开文件 %s 失败: %s", filename, err.Error()), make(http.Header)}
+		}
+		defer file.Close()
+
+		// 创建一个表单文件字段
+		part, err := writer.CreateFormFile("file", filename)
+		if err != nil {
+			return ResponseWrapper{0, fmt.Sprintf("创建表单文件字段失败: %s", err.Error()), make(http.Header)}
+		}
+
+		// 将文件内容复制到表单文件字段中
+		_, err = io.Copy(part, file)
+		if err != nil {
+			return ResponseWrapper{0, fmt.Sprintf("复制文件内容失败: %s", err.Error()), make(http.Header)}
+		}
+	}
+
+	// 关闭写入器，完成表单数据的写入
+	err := writer.Close()
+	if err != nil {
+		return ResponseWrapper{0, fmt.Sprintf("关闭写入器失败: %s", err.Error()), make(http.Header)}
+	}
+
+	// 创建一个 POST 请求
+	req, err := http.NewRequest("POST", httpUrl, body)
+	if err != nil {
+		return ResponseWrapper{0, fmt.Sprintf("创建请求失败: %s", err.Error()), make(http.Header)}
+	}
+
+	// 设置请求头的 Content-Type 为 multipart/form-data 并包含边界信息
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// 添加自定义请求头
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	return request(req, timeout)
+}
+
 func request(req *http.Request, timeout int) ResponseWrapper {
 	wrapper := ResponseWrapper{StatusCode: 0, Body: "", Header: make(http.Header)}
 	client := &http.Client{}
@@ -81,7 +142,7 @@ func request(req *http.Request, timeout int) ResponseWrapper {
 		return wrapper
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		wrapper.Body = fmt.Sprintf("读取HTTP请求返回值失败-%s", err.Error())
 		return wrapper
