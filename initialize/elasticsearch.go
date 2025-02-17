1615495ae043733ac87a6b87c84e1d17c0c927ed
zzsen <296099296@qq.com>
2025-02-17 20:38:53 +0800

    feat: add es service

---
+++ initialize/elasticsearch.go
@@ -0,0 +1,45 @@
+package initialize
+
+import (
+	"context"
+
+	"github.com/zzsen/gin_core/global"
+
+	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	"github.com/zzsen/gin_core/logger"
+)
+
+func InitElasticsearch() {
+	esConfig := elasticsearch.Config{
+		Addresses: global.BaseConfig.Es.Addresses,
+		Password:  global.BaseConfig.Es.Password,
+		Username:  global.BaseConfig.Es.Username,
+	}
+
+	es, err := elasticsearch.NewTypedClient(esConfig)
+
+	if err != nil {
+		logger.Error("[es] Error occurs while creating es client: %s", err)
+		return
+	} else {
+		global.ES = es
+	}
+
+	if err = info(); err != nil {
+		logger.Error("[es] Error occurs while getting es info: %s", err)
+		return
+	}
+}
+
+func info() error {
+	res, err := global.ES.Info().Do(context.Background())
+
+	if err != nil {
+		return err
+	}
+
+	// Print client and server version numbers.
+	logger.Info("[es] Client: %s", elasticsearch.Version)
+	logger.Info("[es] Server: %s", res.Version.Int)
+	return nil
+}
