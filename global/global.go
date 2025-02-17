bfce36026d99ee09ef91b5f8e182ceaf9d0823da
zzsen <296099296@qq.com>
2025-02-17 20:43:21 +0800

    feat: 增加全局发送mq消息方法

--- global/global.go
+++ global/global.go
@@ -1,13 +1,18 @@
 package global
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 
+	elasticsearch "github.com/elastic/go-elasticsearch/v8"
 	"go.uber.org/zap"
 
+	"github.com/zzsen/gin_core/logger"
 	"github.com/zzsen/gin_core/model/config"
 
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
@@ -16,6 +21,7 @@ import (
 var (
 	DB         *gorm.DB
 	DBResolver *gorm.DB
+	ES         *elasticsearch.TypedClient
 	DBList     map[string]*gorm.DB
 	Redis      redis.UniversalClient
 	RedisList  map[string]redis.UniversalClient
@@ -44,4 +50,55 @@ func GetRedisByName(name string) redis.UniversalClient {
 		panic(fmt.Sprintf("redis `%s` no init", name))
 	}
 	return redis
+}
+
+func SendRabbitMqMsg(queueName string, exchangeName string,
+	exchangeType string, routingKey string, message string) {
+	conn, err := amqp.Dial(BaseConfig.RabbitMQ.Url())
+	if err != nil {
+		logger.Error("[消息队列] 连接rabbitMQ失败, error: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		logger.Error("[消息队列] 连接rabbitMQ通道失败, error: %v", err)
+		return
+	}
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		queueName,    // name
+		exchangeType, // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	if err != nil {
+		logger.Error("[消息队列] 定义exchange失败, error: %v", err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = ch.PublishWithContext(ctx,
+		exchangeName, // exchange
+		routingKey,   // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	if err != nil {
+		logger.Error("[消息队列] 消息发布失败, error: %v", err)
+		return
+	}
+
+	logger.Info("[消息队列] 消息发布成功, queueName: %s, exchangeName: %s, exchangeType: %s, routingKey: %s, message: %s",
+		queueName, exchangeName, exchangeType, routingKey, message)
 }
