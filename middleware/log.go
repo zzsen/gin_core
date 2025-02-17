500a225bd1c3b161ea36eaa82e50892448523652
zzsen <296099296@qq.com>
2025-02-17 21:02:29 +0800

    feat: update log middleware

--- middleware/log.go
+++ middleware/log.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"github.com/zzsen/gin_core/core"
 	"github.com/zzsen/gin_core/logger"
 )
@@ -18,23 +20,51 @@ func init() {
 func LogHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Start timer
-		start := time.Now()
-		path := c.Request.URL.Path
-		raw := c.Request.URL.RawQuery
+		startTime := time.Now()
 
 		// Process request
 		c.Next()
 
 		endTime := time.Now()
-		if raw != "" {
-			path = path + "?" + raw
+
+		// 计算请求执行时间
+		latencyTime := endTime.Sub(startTime)
+
+		// 获取请求相关信息
+		reqMethod := c.Request.Method                                     // 请求方式
+		reqUrl := c.Request.RequestURI                                    // 请求路由
+		statusCode := c.Writer.Status()                                   // 状态码
+		clientIP := c.ClientIP()                                          // 请求IP
+		header := c.GetHeader("User-Agent") + "@@" + c.GetHeader("token") // 请求头
+		_ = c.Request.ParseMultipartForm(128)
+		reqForm := c.Request.Form
+		var reqJsonStr string
+
+		// 将请求数据转为 JSON 字符串
+		if len(reqForm) > 0 {
+			reqJsonByte, _ := json.Marshal(reqForm)
+			reqJsonStr = string(reqJsonByte)
 		}
-		//使用高效logger进行打印
-		logger.HInfo("["+c.Request.Method+"] "+path, logger.FieldTime("TimeStamp", endTime),
-			logger.FieldDuration("Latency", endTime.Sub(start)),
-			logger.FieldString("ClientIP", c.ClientIP()),
-			logger.FieldInt("StatusCode", c.Writer.Status()),
-			logger.FieldString("ErrorMessage", c.Errors.ByType(gin.ErrorTypePrivate).String()),
-			logger.FieldInt("BodySize", c.Writer.Size()))
+
+		// 获取请求中的 requestId
+		requestId := c.GetString("requestId")
+
+		// 获取 Gin 中间件中的错误信息
+		var errorsStr string
+		for _, err := range c.Errors.Errors() {
+			errorsStr += err + "; "
+		}
+
+		logger.Logger.WithFields(logrus.Fields{
+			"request_id":   requestId,
+			"status_code":  statusCode,
+			"latency_time": latencyTime,
+			"client_ip":    clientIP,
+			"req_method":   reqMethod,
+			"ua_token":     header,
+			"req_uri":      reqUrl,
+			"body":         reqJsonStr,
+			"err_str":      errorsStr,
+		}).Trace()
 	}
 }
