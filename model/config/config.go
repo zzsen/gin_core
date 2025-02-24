076d9e5c09f9d6fc77e9412a58137d520284f075
zzsen <296099296@qq.com>
2025-02-24 16:34:02 +0800

    feat: 支持多mq连接

--- model/config/config.go
+++ model/config/config.go
@@ -1,15 +1,16 @@
 package config
 
 type BaseConfig struct {
-	System      SystemInfo    `yaml:"system"`
-	Service     ServiceInfo   `yaml:"service"`
-	Log         LoggersConfig `yaml:"log"`
-	Db          DbInfo        `yaml:"db"`
-	DbList      []DbInfo      `yaml:"dbList"`
-	DbResolvers DbResolvers   `yaml:"dbResolvers"`
-	Redis       RedisInfo     `yaml:"redis"`
-	RedisList   []RedisInfo   `yaml:"redisList"`
-	RabbitMQ    RabbitMQInfo  `yaml:"rabbitMQ"`
-	Es          EsInfo        `yaml:"es"`
-	Smtp        SmtpInfo      `yaml:"smtp"`
+	System       SystemInfo       `yaml:"system"`
+	Service      ServiceInfo      `yaml:"service"`
+	Log          LoggersConfig    `yaml:"log"`
+	Db           DbInfo           `yaml:"db"`
+	DbList       []DbInfo         `yaml:"dbList"`
+	DbResolvers  DbResolvers      `yaml:"dbResolvers"`
+	Redis        RedisInfo        `yaml:"redis"`
+	RedisList    []RedisInfo      `yaml:"redisList"`
+	RabbitMQ     RabbitMQInfo     `yaml:"rabbitMQ"`
+	RabbitMQList RabbitMqListInfo `yaml:"rabbitMQList"`
+	Es           EsInfo           `yaml:"es"`
+	Smtp         SmtpInfo         `yaml:"smtp"`
 }
