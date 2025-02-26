11187ddd14b72005ee78ffdbc2148ee3f761cfbb
zhangzhisen <zhangzhisen@wps.cn>
2025-02-26 16:21:12 +0800

    feat: single service support not to start

--- model/config/config.go
+++ model/config/config.go
@@ -4,13 +4,13 @@ type BaseConfig struct {
 	System       SystemInfo       `yaml:"system"`
 	Service      ServiceInfo      `yaml:"service"`
 	Log          LoggersConfig    `yaml:"log"`
-	Db           DbInfo           `yaml:"db"`
+	Db           *DbInfo          `yaml:"db"`
 	DbList       []DbInfo         `yaml:"dbList"`
 	DbResolvers  DbResolvers      `yaml:"dbResolvers"`
-	Redis        RedisInfo        `yaml:"redis"`
+	Redis        *RedisInfo       `yaml:"redis"`
 	RedisList    []RedisInfo      `yaml:"redisList"`
 	RabbitMQ     RabbitMQInfo     `yaml:"rabbitMQ"`
 	RabbitMQList RabbitMqListInfo `yaml:"rabbitMQList"`
-	Es           EsInfo           `yaml:"es"`
+	Es           *EsInfo          `yaml:"es"`
 	Smtp         SmtpInfo         `yaml:"smtp"`
 }
