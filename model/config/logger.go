1e5e4e4a07805fafaa9cb11a37669cd1785efa04
zhangzhisen <zhangzhisen@wps.cn>
2025-03-13 17:11:37 +0800

    feat: config support to transform to db log config

--- model/config/logger.go
+++ model/config/logger.go
@@ -1,24 +1,35 @@
 package config
 
-import "github.com/zzsen/gin_core/constant"
-
 type LoggersConfig struct {
-	FilePath     string                     `yaml:"filePath"`
-	MaxAge       int                        `yaml:"maxAge"`
-	RotationTime int                        `yaml:"rotationTime"`
-	RotationSize int                        `yaml:"rotationSize"`
-	Loggers      []LoggerConfig             `yaml:"loggers"`
-	PrintFile    *constant.LogPrintFileEnum `yaml:"printFile"`
-	PrintFunc    *bool                      `yaml:"printFunc"`
+	FilePath     string         `yaml:"filePath"`
+	MaxAge       int            `yaml:"maxAge"`
+	RotationTime int            `yaml:"rotationTime"`
+	RotationSize int            `yaml:"rotationSize"`
+	Loggers      []LoggerConfig `yaml:"loggers"`
+	PrintCaller  bool           `yaml:"printCaller"`
 }
 
 type LoggerConfig struct {
-	Level        string                     `yaml:"level"`
-	FileName     string                     `yaml:"fileName"`
-	FilePath     string                     `yaml:"filePath"`
-	MaxAge       int                        `yaml:"maxAge"`
-	RotationTime int                        `yaml:"rotationTime"`
-	RotationSize int                        `yaml:"rotationSize"`
-	PrintFile    *constant.LogPrintFileEnum `yaml:"printFile"`
-	PrintFunc    *bool                      `yaml:"printFunc"`
+	Level        string `yaml:"level"`
+	FileName     string `yaml:"fileName"`
+	FilePath     string `yaml:"filePath"`
+	MaxAge       int    `yaml:"maxAge"`
+	RotationTime int    `yaml:"rotationTime"`
+	RotationSize int    `yaml:"rotationSize"`
+}
+
+func (loggersConfig LoggersConfig) ToDbLoggerConfig() LoggersConfig {
+	logSubfix := "DB"
+	if loggersConfig.FilePath != "" {
+		loggersConfig.FilePath = loggersConfig.FilePath + logSubfix
+	}
+	for i := range loggersConfig.Loggers {
+		if loggersConfig.Loggers[i].FileName != "" {
+			loggersConfig.Loggers[i].FileName = loggersConfig.Loggers[i].FileName + logSubfix
+		}
+		if loggersConfig.Loggers[i].FilePath != "" {
+			loggersConfig.Loggers[i].FilePath = loggersConfig.Loggers[i].FilePath + logSubfix
+		}
+	}
+	return loggersConfig
 }
