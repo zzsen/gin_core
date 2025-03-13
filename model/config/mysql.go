dbfea39e40b1317ccbb6da8e2ae9cb8513133d4c
zhangzhisen <zhangzhisen@wps.cn>
2025-03-13 17:28:03 +0800

    feat: set default db slowThreshold

--- model/config/mysql.go
+++ model/config/mysql.go
@@ -15,7 +15,7 @@ type DbInfo struct {
 	MaxOpenConns              int      `yaml:"maxOpenConns"`              // 打开到数据库的最大连接数
 	Migrate                   string   `yaml:"migrate"`                   // 每次启动时更新数据库表的方式 update:增量更新表，create:删除所有表再重新建表, 其他则不执行任何动作
 	LogLevel                  *int     `yaml:"logLevel"`                  // 日志级别（1-关闭所有日志，2-仅输出错误日志，3-输出错误日志和慢查询，4-输出错误日志和慢查询日志和所有sql）
-	SlowThreshold             int      `yaml:"slowThreshold"`             // 慢查询阈值（单位：毫秒）
+	SlowThreshold             *int     `yaml:"slowThreshold"`             // 慢查询阈值（单位：毫秒）
 	IgnoreRecordNotFoundError *bool    `yaml:"ignoreRecordNotFoundError"` // 忽略记录未找到错误
 	Tables                    []string `yaml:"tables"`                    // 走该库查询的数据表
 	TablePrefix               string   `yaml:"tablePrefix"`               // 表名前缀
