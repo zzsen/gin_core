8be0abb7ba08643b8c55f05b9eefec4013ace6a6
zhangzhisen <zhangzhisen@wps.cn>
2025-03-13 17:09:41 +0800

    feat: remove constant log config

--- constant/config.go
+++ constant/config.go
@@ -17,14 +17,3 @@ const CustomConfigFileNamePrefix = "config."
 
 // 自定义配置文件名称后缀
 const CustomConfigFileNameSuffix = ".yml"
-
-type LogPrintFileEnum int
-
-const (
-	// 不打印文件信息
-	NoPrintFile LogPrintFileEnum = iota
-	// 打印相对文件路径
-	PrintRelativeFile
-	// 打印绝对文件路径
-	PrintAbsoluteFile
-)
