6807aed51f5e44d01619c77938cf1478fb2f3278
zzsen <296099296@qq.com>
2025-04-03 00:49:45 +0800

    feat: use mysql initial base function

--- initialize/mysql.go
+++ initialize/mysql.go
@@ -1,123 +1,61 @@
 package initialize
 
 import (
-	"reflect"
-	"time"
+	"fmt"
 
-	"github.com/zzsen/gin_core/constant"
 	"github.com/zzsen/gin_core/global"
 	"github.com/zzsen/gin_core/logger"
 	"github.com/zzsen/gin_core/model/config"
-	gormLogger "gorm.io/gorm/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 )
 
 var tableEntity []interface{}
 
 func InitDB() {
 	if global.BaseConfig.Db == nil {
-		logger.Error("[db] single db has no config, please check config")
+		logger.Error("[db] 未找到配置, 请检查配置")
 		return
 	}
-	global.DB = initSingleDB(*global.BaseConfig.Db)
+	dbClient, err := initSingleDB(*global.BaseConfig.Db)
+	if err != nil {
+		panic(fmt.Errorf("[db] 初始化db失败, %s", err.Error()))
+	}
+	global.DB = dbClient
 	logger.Info("[db] db已初始化")
 }
 
 func InitDBList() {
 	global.DBList = make(map[string]*gorm.DB)
 	for _, dbConfig := range global.BaseConfig.DbList {
-		global.DBList[dbConfig.AliasName] = initSingleDB(dbConfig)
+		dbClient, err := initSingleDB(dbConfig)
+		if err != nil {
+			panic(fmt.Errorf("[db] 初始化db [%s] 失败, %s", dbConfig.AliasName, err.Error()))
+		}
+		global.DBList[dbConfig.AliasName] = dbClient
 	}
+	logger.Info("[db] db列表已初始化")
 }
 
-func initSingleDB(dbConfig config.DbInfo) *gorm.DB {
+func initSingleDB(dbConfig config.DbInfo) (*gorm.DB, error) {
 	var err error
 
-	gormConfig := &gorm.Config{
-		DisableForeignKeyConstraintWhenMigrating: true,
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,                 // 使用单数表名，启用该选项后，`User` 表将是`user`
-			TablePrefix:   dbConfig.TablePrefix, // 表名前缀，`User`表为`t_users`
-			//NameReplacer:  strings.NewReplacer("CID", "Cid"), // 在转为数据库名称之前，使用NameReplacer更改结构/字段名称。
-		},
-	}
-
-	// 初始化日志记录器
-	loggerConfig := global.BaseConfig.Log.ToDbLoggerConfig()
-	dbLogger := logger.InitLogger(loggerConfig)
-	// 是否忽略记录未找到错误
-	ignoreRecordNotFoundError := true
-	if dbConfig.IgnoreRecordNotFoundError != nil {
-		ignoreRecordNotFoundError = *dbConfig.IgnoreRecordNotFoundError
-	}
-	// 日志级别
-	logLevel := gormLogger.Warn
-	if dbConfig.LogLevel != nil {
-		logLevel = gormLogger.LogLevel(*dbConfig.LogLevel)
-	}
-	// 慢查询阈值
-	slowThreshold := constant.DefaultDBSlowThreshold
-	if dbConfig.SlowThreshold != nil {
-		slowThreshold = *dbConfig.SlowThreshold
-	}
-	gormConfig.Logger = gormLogger.New(
-		dbLogger,
-		gormLogger.Config{
-			SlowThreshold:             time.Duration(slowThreshold) * time.Millisecond, // 慢查询阈值, 单位: 毫秒
-			LogLevel:                  logLevel,                                        // 日志级别
-			IgnoreRecordNotFoundError: ignoreRecordNotFoundError,                       // 忽略ErrRecordNotFound（记录未找到）错误
-			Colorful:                  true,                                            // 彩色打印
-		},
-	)
+	gormConfig := initGormConfig(dbConfig)
 
 	DB, err := gorm.Open(mysql.New(mysql.Config{
 		DSN: dbConfig.Dsn(),
 	}), gormConfig)
 	if err != nil {
-		logger.Error("[db] error occurs while initializing db, error: %v", err)
-		return nil
+		return nil, fmt.Errorf("初始化 mysql client 失败: %v", err)
 	}
 
-	//创建前把createTime和updateTime字段填充好默认值
-	DB.Callback().Create().Before("gorm:create").Register("update_create_time", func(db *gorm.DB) {
-		if db.Statement.Schema == nil {
-			return
-		}
-		timeFieldsToInit := []string{"CreateTime", "UpdateTime"}
-		for _, field := range timeFieldsToInit {
+	initDBCallbacks(DB)
 
-			if timeField := db.Statement.Schema.LookUpField(field); timeField != nil {
-				switch db.Statement.ReflectValue.Kind() {
-				case reflect.Slice, reflect.Array:
-					for i := 0; i < db.Statement.ReflectValue.Len(); i++ {
-						if _, isZero := timeField.ValueOf(db.Statement.Context, db.Statement.ReflectValue.Index(i)); isZero {
-							timeField.Set(db.Statement.Context, db.Statement.ReflectValue.Index(i), time.Now())
-						}
-					}
-				case reflect.Struct:
-					if _, isZero := timeField.ValueOf(db.Statement.Context, db.Statement.ReflectValue); isZero {
-						timeField.Set(db.Statement.Context, db.Statement.ReflectValue, time.Now())
-					}
-				}
-			}
-		}
-	})
-
-	SqlDB, err := DB.DB()
-	if err != nil {
-		logger.Error("[db] get sqlDB err: %v", err)
-		return nil
-	}
-	SqlDB.SetMaxIdleConns(max(dbConfig.MaxIdleConns, 10))
-	SqlDB.SetMaxOpenConns(max(dbConfig.MaxOpenConns, 100))
-	SqlDB.SetConnMaxIdleTime(time.Duration(max(dbConfig.ConnMaxIdleTime, 60)) * time.Second)
-	SqlDB.SetConnMaxLifetime(time.Duration(max(dbConfig.ConnMaxLifetime, 60)) * time.Second)
+	initDBConnConfig(DB, dbConfig)
 
 	Migrate(DB, dbConfig.Migrate)
-	return DB
+	return DB, nil
 }
 
 func RegisterTable(table interface{}) {
--- initialize/mysql_resolver.go
+++ initialize/mysql_resolver.go
@@ -1,84 +1,45 @@
 package initialize
 
 import (
-	"reflect"
+	"fmt"
 	"time"
 
-	"github.com/zzsen/gin_core/constant"
 	"github.com/zzsen/gin_core/global"
 	"github.com/zzsen/gin_core/logger"
 	"github.com/zzsen/gin_core/model/config"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	gormLogger "gorm.io/gorm/logger"
-	"gorm.io/gorm/schema"
 	"gorm.io/plugin/dbresolver"
 )
 
 func InitDBResolver() {
 	if len(global.BaseConfig.DbResolvers) > 0 {
-		global.DBResolver = initMultiDB(global.BaseConfig.DbResolvers)
+		dbClient, err := initMultiDB(global.BaseConfig.DbResolvers)
+		if err != nil {
+			panic(fmt.Errorf("[db] 初始化db resolver失败, %s", err.Error()))
+		}
+		global.DBResolver = dbClient
 		logger.Info("[db] db resolver已初始化")
 	}
 }
 
-func initMultiDB(dbResolvers config.DbResolvers) *gorm.DB {
+func initMultiDB(dbResolvers config.DbResolvers) (*gorm.DB, error) {
 	defaultDBConfig := dbResolvers.DefaultConfig()
-	var err error
 
-	gormConfig := &gorm.Config{
-		DisableForeignKeyConstraintWhenMigrating: true,
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,                        // 使用单数表名，启用该选项后，`User` 表将是`user`
-			TablePrefix:   defaultDBConfig.TablePrefix, // 表名前缀，`User`表为`t_users`
-			//NameReplacer:  strings.NewReplacer("CID", "Cid"), // 在转为数据库名称之前，使用NameReplacer更改结构/字段名称。
-		},
-	}
-
-	// 初始化日志记录器
-	loggerConfig := global.BaseConfig.Log.ToDbLoggerConfig()
-	dbLogger := logger.InitLogger(loggerConfig)
-
-	// 是否忽略记录未找到错误
-	ignoreRecordNotFoundError := true
-	if defaultDBConfig.IgnoreRecordNotFoundError != nil {
-		ignoreRecordNotFoundError = *defaultDBConfig.IgnoreRecordNotFoundError
-	}
-	// 日志级别
-	logLevel := gormLogger.Warn
-	if defaultDBConfig.LogLevel != nil {
-		logLevel = gormLogger.LogLevel(*defaultDBConfig.LogLevel)
-	}
-	// 慢查询阈值
-	slowThreshold := constant.DefaultDBSlowThreshold
-	if defaultDBConfig.SlowThreshold != nil {
-		slowThreshold = *defaultDBConfig.SlowThreshold
-	}
-
-	gormConfig.Logger = gormLogger.New(
-		dbLogger,
-		gormLogger.Config{
-			SlowThreshold:             time.Duration(slowThreshold) * time.Millisecond, // 慢查询阈值
-			LogLevel:                  logLevel,                                        // 日志级别
-			IgnoreRecordNotFoundError: ignoreRecordNotFoundError,                       // 忽略ErrRecordNotFound（记录未找到）错误
-			Colorful:                  true,                                            // 彩色打印
-		},
-	)
+	gormConfig := initGormConfig(defaultDBConfig)
 
 	DB, err := gorm.Open(mysql.New(mysql.Config{
 		DSN: defaultDBConfig.Dsn(),
 	}), gormConfig)
 	if err != nil {
-		logger.Error("error occurs while initializing db resolver, error: %v", err)
-		return nil
+		return nil, fmt.Errorf("初始化 mysql client 失败: %v", err)
 	}
 
 	resolverPlugin := &dbresolver.DBResolver{}
 	for _, resolver := range dbResolvers {
 		if !resolver.IsValid() {
-			logger.Error("exists invalid db resolver config, please check config")
-			return nil
+			return nil, fmt.Errorf("无效的db resolver配置, 请检查配置")
 		}
 		resolverPlugin.Register(dbresolver.Config{
 			Sources:           resolver.SourceConfigs(),
@@ -86,40 +47,17 @@ func initMultiDB(dbResolvers config.DbResolvers) *gorm.DB {
 			TraceResolverMode: true,
 		}, resolver.Tables...)
 	}
+
 	resolverPlugin.SetMaxIdleConns(max(defaultDBConfig.MaxIdleConns, 10))
 	resolverPlugin.SetMaxOpenConns(max(defaultDBConfig.MaxOpenConns, 100))
 	resolverPlugin.SetConnMaxIdleTime(time.Duration(max(defaultDBConfig.ConnMaxIdleTime, 60)) * time.Second)
 	resolverPlugin.SetConnMaxLifetime(time.Duration(max(defaultDBConfig.ConnMaxLifetime, 60)) * time.Second)
 
 	if err := DB.Use(resolverPlugin); err != nil {
-		logger.Error("error occurs while db trying to use db resolver, error: %v", err)
-		return nil
+		return nil, fmt.Errorf("启用db resolver plugin失败: %v", err)
 	}
 
-	//创建前把createTime和updateTime字段填充好默认值
-	DB.Callback().Create().Before("gorm:create").Register("update_create_time", func(db *gorm.DB) {
-		if db.Statement.Schema == nil {
-			return
-		}
-		timeFieldsToInit := []string{"CreateTime", "UpdateTime"}
-		for _, field := range timeFieldsToInit {
+	initDBCallbacks(DB)
 
-			if timeField := db.Statement.Schema.LookUpField(field); timeField != nil {
-				switch db.Statement.ReflectValue.Kind() {
-				case reflect.Slice, reflect.Array:
-					for i := 0; i < db.Statement.ReflectValue.Len(); i++ {
-						if _, isZero := timeField.ValueOf(db.Statement.Context, db.Statement.ReflectValue.Index(i)); isZero {
-							timeField.Set(db.Statement.Context, db.Statement.ReflectValue.Index(i), time.Now())
-						}
-					}
-				case reflect.Struct:
-					if _, isZero := timeField.ValueOf(db.Statement.Context, db.Statement.ReflectValue); isZero {
-						timeField.Set(db.Statement.Context, db.Statement.ReflectValue, time.Now())
-					}
-				}
-			}
-		}
-	})
-
-	return DB
+	return DB, nil
 }
