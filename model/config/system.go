1465a57f5101c80481a82c6b553379ce9731f469
zzsen <296099296@qq.com>
2025-04-03 00:50:11 +0800

    feat: add use etcd config

--- model/config/system.go
+++ model/config/system.go
@@ -5,6 +5,7 @@ type SystemInfo struct {
 	UseRedis    bool `yaml:"useRedis"`    // 是否启用redis
 	UseMysql    bool `yaml:"useMysql"`    // 是否启用mysql
 	UseEs       bool `yaml:"useEs"`       // 是否启用elasticsearch
+	UseEtcd     bool `yaml:"useEtcd"`     // 是否启用etcd
 	UseRabbitMQ bool `yaml:"useRabbitMQ"` // 是否启用rabbitmq
 	UseSchedule bool `yaml:"useSchedule"` // 是否启用定时任务
 }
