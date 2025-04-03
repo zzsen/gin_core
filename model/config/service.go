7e405e7656aec5be693ffa5e24f66270e78dac89
zzsen <296099296@qq.com>
2025-04-03 19:30:59 +0800

    feat: add api timeout config

--- model/config/service.go
+++ model/config/service.go
@@ -7,6 +7,7 @@ type ServiceInfo struct {
 	SessionExpire int      `yaml:"sessionExpire"` // 缓存的有效时长
 	SessionPrefix string   `yaml:"sessionPrefix"` // redis中缓存前缀
 	Middlewares   []string `yaml:"middlewares"`   // 中间件，顺序对应中间件调用顺序
+	ApiTimeout    int      `yaml:"apiTimeout"`    // api超时时间
 	ReadTimeout   int      `yaml:"readTimeout"`   // 读取超时时间
 	WriteTimeout  int      `yaml:"writeTimeout"`  // 写入超时时间
 	PprofPort     *int     `yaml:"pprofPort"`     // pprof服务端口
