5f2f3fd0dc3daad9bfb98f977c9ae24b24acab53
zhangzhisen <zhangzhisen@kingsoft.com>
2025-07-22 16:16:01 +0800

    feat: es search demo

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 
+	"github.com/elastic/go-elasticsearch/v9/typedapi/core/search"
+	"github.com/elastic/go-elasticsearch/v9/typedapi/types"
 	"github.com/zzsen/gin_core/app"
 	"github.com/zzsen/gin_core/core"
 	"github.com/zzsen/gin_core/logger"
@@ -26,7 +31,40 @@ func getCustomRouter1() func(e *gin.Engine) {
 	return func(e *gin.Engine) {
 		r := e.Group("customRouter1")
 		r.GET("test", func(c *gin.Context) {
-			response.Ok(c)
+			type TestData struct {
+				Id   int    `json:"id"`
+				Name string `json:"name"`
+			}
+			testDatas := []TestData{}
+			size := 2
+			resp, _ := app.ES.Search().Index("test_data*").Request(&search.Request{
+				Query: &types.Query{
+					Match: map[string]types.MatchQuery{
+						"extension": types.MatchQuery{
+							Query: ".html",
+						},
+					},
+				},
+				Size: &size,
+			}).Do(context.Background())
+
+			fmt.Println(resp.Hits.Total.Value)
+
+			// 遍历命中结果
+			for _, hit := range resp.Hits.Hits {
+				// 声明结构体变量
+				var testData TestData
+
+				if err := json.Unmarshal(hit.Source_, &testData); err != nil {
+					log.Printf("解析 %s 失败: %v", hit.Id_, err)
+					continue
+				}
+				testDatas = append(testDatas, testData)
+			}
+			response.OkWithData(c, gin.H{
+				"total": resp.Hits.Total.Value,
+				"data":  testDatas,
+			})
 		})
 	}
 }
